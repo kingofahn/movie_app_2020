fb3fc8555334a72516e40c0bb2bcdbb8a6b4f9fe
Hyungjun Ahn <kingofahn@Hyungjuns-MacBook-Pro.local>
2020-06-12 17:27:15 +0900

    Updates

--- precache-manifest.f324f81021bd5bbba51e18b1f3fd39cd.js
+++ precache-manifest.f324f81021bd5bbba51e18b1f3fd39cd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "36d82164c319f2a69b079dd444de843f",
+    "revision": "8029a1eba3f81d3fc7a3e38e352ca5a8",
     "url": "/movie_app_2020/index.html"
   },
   {
-    "revision": "af0a557fe8b4e552ddc7",
-    "url": "/movie_app_2020/static/css/main.19835dfe.chunk.css"
+    "revision": "dbcdb21d5df0fe85ab4c",
+    "url": "/movie_app_2020/static/css/main.387c0d30.chunk.css"
   },
   {
     "revision": "da5d31b2053ef86a8fa6",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app_2020/static/js/2.1ec6ec2b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "af0a557fe8b4e552ddc7",
+    "revision": "dbcdb21d5df0fe85ab4c",
     "url": "/movie_app_2020/static/js/main.e9af58ce.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app_2020/precache-manifest.277356527a72a487d707c19809e88bb7.js"
+  "/movie_app_2020/precache-manifest.f324f81021bd5bbba51e18b1f3fd39cd.js"
 );
 
 self.addEventListener('message', (event) => {
